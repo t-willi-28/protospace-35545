b7cc4372b3a0db85bafb08e7b59fc06554191090
t-willi-28 <taku.0628william@gmail.com>
2021-05-10 15:26:32 +0900

    修正２

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -31,8 +31,7 @@ class PrototypesController < ApplicationController
 
   def update
     @prototype = Prototype.find(params[:id])
-    if @prototype.save
-      @prototype.update(prototype_params)
+    if @prototype.update(prototype_params)
       redirect_to prototype_path
     else
       render :edit
